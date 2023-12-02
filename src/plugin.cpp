9490a2c491d71ed3b560bb66347b1e12a0803d24
killcerr <93478098+killcerr@users.noreply.github.com>
2023-12-02 11:32:18 +0800

    Add files via upload

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -12,10 +12,41 @@ extern Logger logger;
 
 /**
  * @brief The entrypoint of the plugin. DO NOT remove or rename this function.
- *        
+ *
  */
-void PluginInit()
-{
-    // Your code here
-    logger.info("Hello, world!");
+void PluginInit() { logger.info("MinecartSpeeedPlus by killcerr loaded"); }
+
+#include <llapi/mc/RailMovementUtility.hpp>
+#include <llapi/mc/Vec3.hpp>
+
+bool flag = false;
+
+TStaticHook(Vec3,
+            "?calculateGoldenRailSpeedIncrease@RailMovementUtility@@SA?AVVec3@@"
+            "AEBVIConstBlockSource@@AEBVBlockPos@@HV2@@Z",
+            RailMovementUtility, class IConstBlockSource const &a,
+            class BlockPos const &b, int c, class Vec3 d) {
+  auto res = original(a, b, c, d);
+  logger.info("calculateGoldenRailSpeedIncrease::speed:{},{},{}", res.x, res.y,
+              res.z);
+  flag = true;
+  return res;
+}
+
+TStaticHook(Vec3,
+            "?calculateMoveVelocity@RailMovementUtility@@SA?"
+            "AVVec3@@AEBVBlock@@HM_NAEAV2@AEA_N3AEBV?$function@$$A6A_"
+            "NAEAVVec3@@@Z@std@@@Z",
+            RailMovementUtility, class Block const &a, int b, float c, bool d,
+            class Vec3 &e, bool &f, bool &g,
+            class std::function<bool(class Vec3 &)> const &h) {
+  auto res = original(a, b, c, d, e, f, g, h);
+  if (!flag)
+    res *= 5;
+  else {
+    flag = false;
+    res *= 15;
+  }
+  logger.info("calculateMoveVelocity::speed:{},{},{}", res.x, res.y, res.z);
+  return res;
 }
--- src/version.h
+++ src/version.h
@@ -10,24 +10,21 @@
 #define PLUGIN_VERSION_BETA 1
 #define PLUGIN_VERSION_RELEASE 2
 
-
 // ***************************************************************************************
 // ***************************************************************************************
 // ***************************************************************************************
 
-// Please FILL UP the content in this area before developing your plugin, and DO NOT modify any code
-// outside this area.
-
-static_assert(false, "Please modify your plugin information below and delete this line!");
+// Please FILL UP the content in this area before developing your plugin, and DO
+// NOT modify any code outside this area.
 
 // The basic descriptions of your plugin
-// Note that the the plugin name CAN ONLY CONTAIN Enlish alphabet, digit, dash(-) and
-// underscore(_). 
-#define PLUGIN_NAME "HelloWorldPrinter"
+// Note that the the plugin name CAN ONLY CONTAIN Enlish alphabet, digit,
+// dash(-) and underscore(_).
+#define PLUGIN_NAME "MinecartSpeeedPlus"
 // Describe what your plugin does
 // We recommend you start the introduction with a third-person singular verb.
-#define PLUGIN_INTRODUCTION "Prints \"Hello, World!\" in the console."
-#define PLUGIN_AUTHOR "Me"
+#define PLUGIN_INTRODUCTION ""
+#define PLUGIN_AUTHOR "killcerr"
 
 // The version of your plugin. If you do not know how to set it, please refer
 // to https://semver.org/ .
@@ -37,20 +34,18 @@ static_assert(false, "Please modify your plugin information below and delete thi
 #define PLUGIN_VERSION_BUILD 0
 #define PLUGIN_VERSION_ACTIONS 0
 
-// The stage of your plugin, which can be PLUGIN_VERSION_DEV, PLUGIN_VERSION_BETA
-// and PLUGIN_VERSION_RELEASE
+// The stage of your plugin, which can be PLUGIN_VERSION_DEV,
+// PLUGIN_VERSION_BETA and PLUGIN_VERSION_RELEASE
 #define PLUGIN_VERSION_STATUS PLUGIN_VERSION_DEV
 
 // The target protocol version of your plugin. If you do not know how to set it,
-// please start the latest version of LiteLoaderBDS and get the protocol version.
-// If you are confident about the version compatibility of your plugin, you can
-// just comment this line (NOT RECOMMENDED).
-#define TARGET_BDS_PROTOCOL_VERSION 582
+// please start the latest version of LiteLoaderBDS and get the protocol
+// version. If you are confident about the version compatibility of your plugin,
+// you can just comment this line (NOT RECOMMENDED).
 
 // ***************************************************************************************
 // ***************************************************************************************
 // ***************************************************************************************
-
 
 // File version information, which is used in Resource.rc
 #define __TO_VERSION_STRING(ver) #ver
@@ -60,19 +55,25 @@ static_assert(false, "Please modify your plugin information below and delete thi
 
 #define PLUGIN_FILE_VERSION_FLAG VS_FF_DEBUG
 #define PLUGIN_LLVERSION_STATUS ll::Version::Beta
-#define PLUGIN_FILE_VERSION_STRING TO_VERSION_STRING(PLUGIN_VERSION_MAJOR.PLUGIN_VERSION_MINOR.PLUGIN_VERSION_REVISION.PLUGIN_VERSION_ACTIONS BETA)
+#define PLUGIN_FILE_VERSION_STRING                                             \
+  TO_VERSION_STRING(PLUGIN_VERSION_MAJOR.PLUGIN_VERSION_MINOR                  \
+                        .PLUGIN_VERSION_REVISION.PLUGIN_VERSION_ACTIONS BETA)
 
 #elif PLUGIN_VERSION_STATUS == PLUGIN_VERSION_DEV
 
 #define PLUGIN_FILE_VERSION_FLAG VS_FF_DEBUG
 #define PLUGIN_LLVERSION_STATUS ll::Version::Dev
-#define PLUGIN_FILE_VERSION_STRING TO_VERSION_STRING(PLUGIN_VERSION_MAJOR.PLUGIN_VERSION_MINOR.PLUGIN_VERSION_REVISION.PLUGIN_VERSION_ACTIONS DEV)
+#define PLUGIN_FILE_VERSION_STRING                                             \
+  TO_VERSION_STRING(PLUGIN_VERSION_MAJOR.PLUGIN_VERSION_MINOR                  \
+                        .PLUGIN_VERSION_REVISION.PLUGIN_VERSION_ACTIONS DEV)
 
 #else
 
 #define PLUGIN_FILE_VERSION_FLAG 0x0L
 #define PLUGIN_LLVERSION_STATUS ll::Version::Release
-#define PLUGIN_FILE_VERSION_STRING TO_VERSION_STRING(PLUGIN_VERSION_MAJOR.PLUGIN_VERSION_MINOR.PLUGIN_VERSION_REVISION.PLUGIN_VERSION_ACTIONS)
+#define PLUGIN_FILE_VERSION_STRING                                             \
+  TO_VERSION_STRING(PLUGIN_VERSION_MAJOR.PLUGIN_VERSION_MINOR                  \
+                        .PLUGIN_VERSION_REVISION.PLUGIN_VERSION_ACTIONS)
 
 #endif
 
@@ -85,5 +86,7 @@ static_assert(false, "Please modify your plugin information below and delete thi
 #define FILE_VERSION_ORIGINAL_FILENAME PLUGIN_NAME ".dll"
 #define FILE_VERSION_PRODUCT_NAME FILE_VERSION_INTERNAL_NAME
 #define FILE_VERSION_PRODUCT_VERSION_STRING PLUGIN_FILE_VERSION_STRING
-#define FILE_VERSION_FILE_VERSION PLUGIN_VERSION_MAJOR, PLUGIN_VERSION_MINOR, PLUGIN_VERSION_REVISION, PLUGIN_VERSION_BUILD
+#define FILE_VERSION_FILE_VERSION                                              \
+  PLUGIN_VERSION_MAJOR, PLUGIN_VERSION_MINOR, PLUGIN_VERSION_REVISION,         \
+      PLUGIN_VERSION_BUILD
 #define FILE_VERSION_PRODUCT_VERSION FILE_VERSION_FILE_VERSION
