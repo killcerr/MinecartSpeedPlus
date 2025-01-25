d5dc7377c1d6b331e2785cb3d773b884fee50a79
killcerr <kill_cerr@outlook.com>
2025-01-25 19:36:55 +0800

    feat: add MinecartPushHook

--- src/MinecartSpeedPlus.cpp
+++ src/MinecartSpeedPlus.cpp
@@ -12,13 +12,16 @@ struct {
     float CommonRailMul;
     float ClimbRailMul;
     bool  EnableMinecartAABBHook;
+    bool  EnableMinecartPushHook;
 } cfg{
-    .version                = 0,
+    .version                = 1,
     .GoldenRailMul          = 1.0,
     .CommonRailMul          = 1.0,
     .ClimbRailMul           = 1.0,
     .EnableMinecartAABBHook = false,
+    .EnableMinecartPushHook = false,
 };
+
 
 void ModInit() {
     const auto configDir = mod::Mod::getInstance().getSelf().getConfigDir();
@@ -81,15 +84,48 @@ LL_AUTO_TYPE_STATIC_HOOK /*NOLINT*/ (
     return res;
 }
 
+#include <ll/api/service/GamingStatus.h>
+#include <mc/entity/components_json_legacy/PushableComponent.h>
 #include <mc/world/actor/Actor.h>
 #include <mc/world/actor/ActorType.h>
 #include <mc/world/phys/AABB.h>
+
+
 LL_AUTO_TYPE_INSTANCE_HOOK(minecartAABBHook, HookPriority::Normal, Actor, &Actor::getAABB, AABB const&) /*NOLINT*/ {
     static AABB box{Vec3::ZERO(), Vec3::ZERO()};
     if (cfg.EnableMinecartAABBHook
         && (static_cast<int>(getEntityTypeId()) & static_cast<int>(ActorType::Minecart)) == 0x80000)
         return box;
     return origin();
+}
+// using RetT = ::std::pair<::Vec3, ::Vec3>;
+LL_AUTO_TYPE_INSTANCE_HOOK(
+    fixPullToRideHook,
+    HookPriority::Normal,
+    PushableComponent,
+    &PushableComponent::push,
+    void,
+    ::Actor& owner,
+    ::Actor& other,
+    bool     pushSelfOnly
+) /*NOLINT*/
+{
+    if (!cfg.EnableMinecartPushHook) {
+        return origin(owner, other, pushSelfOnly);
+    }
+    using enum ActorType;
+    if (((static_cast<int>(owner.getEntityTypeId()) & static_cast<int>(ActorType::Minecart)) == 0x80000)) {
+        if (owner.canAddPassenger(other)
+            && (other.hasCategory(ActorCategory::Mob) && !other.isPlayer() && !other.hasType(AbstractArrow)
+                && !other.hasCategory(ActorCategory::Minecart) && !other.hasCategory(ActorCategory::Boat)
+                && !other.hasType(ItemEntity) && !other.hasType(Projectile) && !other.hasType(PrimedTnt)
+                && !other.hasType(EnderCrystal) && !other.hasType(Enderpearl) && !other.hasType(Dragon)
+                && !other.hasType(WitherBoss))) {
+            owner.addPassenger(other);
+        }
+        return;
+    }
+    return origin(owner, other, pushSelfOnly);
 }
 
 void ModDeinit() {
